cf5fe22b2c95b5797d70c40a70a413559fdd9a16
Caio <Caio.abreu@sou.inteli.edu.br>
2023-03-30 20:17:14 -0300

    Update: Comments added
    Comments added and code cleaned

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ app = Flask(__name__)
 
 DATABASE = 'database.db'
 
+# Função para conectar com o banco de dados
 def get_db():
     return sqlite3.connect(DATABASE)
 
+# Função para fechar a conexão com o banco de dados
 def close_db(conn):
     conn.close()
 
+# Função para executar uma query no banco de dados
 def execute_query(query, args=(), commit=False):
     conn = get_db()
     with conn:
@@ -20,13 +23,15 @@ def execute_query(query, args=(), commit=False):
             conn.commit()
         return cursor
 
+# Rota principal, que exibe a última linha da tabela 'axis'
 @app.route('/')
 def index():
     query = 'SELECT id, x, y, z, r FROM axis ORDER BY id DESC LIMIT 1'
-    ultima_linha = execute_query(query).fetchone()
-    x, y, z, r = ultima_linha[1:]
+    last_entry = execute_query(query).fetchone()
+    x, y, z, r = last_entry[1:]
     return render_template('index.html', x=x, y=y, z=z, r=r)
 
+# Rota para receber os dados do formulário via POST e inseri-los na tabela 'axis'
 @app.route('/data', methods=['POST'])
 def post():
     query = 'INSERT INTO axis (x, y, z, r) VALUES (?, ?, ?, ?)'
@@ -34,13 +39,14 @@ def post():
     execute_query(query, args, commit=True)
     return redirect('/')
 
+# Rota para retornar a última linha da tabela 'axis' como um objeto JSON
 @app.route('/data', methods=['GET'])
 def get():
     query = 'SELECT id, x, y, z, r FROM axis ORDER BY id DESC LIMIT 1'
-    ultima_linha = execute_query(query).fetchone()
-    my_dict = {'id': ultima_linha[0], 'x': ultima_linha[1], 'y': ultima_linha[2], 'z': ultima_linha[3], 'r': ultima_linha[4]}
-    return jsonify(my_dict)
+    last_entry = execute_query(query).fetchone()
+    response = {'id': last_entry[0], 'x': last_entry[1], 'y': last_entry[2], 'z': last_entry[3], 'r': last_entry[4]}
+    return jsonify(response)
 
-
+# Executa o servidor Flask
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,11 @@
+// Essa função atualiza os valores finais do gráfico somando os valores do eixo X, Y, Z e R.
 function updateFinalValues() {
+    // Seleciona os elementos de texto dos eixos X, Y, Z e R.
     const inputs = ['#x', '#y', '#z', '#r'];
-    const finals = ['#x_final', '#y_final', '#z_final', '#r_final'];
+    // Seleciona os elementos de texto dos valores finais dos eixos X, Y, Z e R.
+    const finals = ['#last_x', '#last_y', '#last_z', '#last_r'];
 
+    // Percorre os elementos de texto dos eixos e soma seus valores aos valores finais correspondentes.
     inputs.forEach((input, i) => {
         const inputVal = parseInt($(input).text());
         const finalVal = parseInt($(finals[i]).text()) + inputVal;
@@ -9,32 +13,40 @@ function updateFinalValues() {
     });
 }
 
+// Essa função envia os valores finais atualizados para o servidor através de uma requisição AJAX.
 function sendUpdatedValuesToServer() {
-    const finalVals = ['#x_final', '#y_final', '#z_final', '#r_final'];
+    // Seleciona os elementos de texto dos valores finais dos eixos X, Y, Z e R.
+    const finalVals = ['#last_x', '#last_y', '#last_z', '#las'];
+
+    // Cria um objeto com os valores finais de cada eixo.
     const data = finalVals.reduce((acc, curr) => {
         acc[curr.slice(1)] = parseInt($(curr).text());
         return acc;
     }, {});
 
+    // Realiza uma requisição AJAX POST para enviar os dados para o servidor.
     $.ajax({
         url: '/data',
         type: 'POST',
         data: data,
         success: function(response) {
-            alert('Texto enviado com sucesso!');
+            alert('Dados enviados com sucesso!');
             location.reload();
         },
         error: function(xhr, status, error) {
-            alert('Erro ao enviar texto: ' + error);
+            alert('Erro ao enviar dados: ' + error);
             location.reload();
         }
     });
 }
 
 $(document).ready(function() {
+    // Chama a função que atualiza os valores finais quando a página é carregada.
     updateFinalValues();
 
+    // Adiciona um listener de clique no botão de atualização.
     $('#update').click(function() {
+        // Chama a função que atualiza os valores finais e envia os dados para o servidor.
         updateFinalValues();
         sendUpdatedValuesToServer();
     });
