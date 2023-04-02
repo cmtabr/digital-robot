120e902e7ecde5c8b188d6f996eb7b3f0fbbda59
Caio <Caio.abreu@sou.inteli.edu.br>
2023-04-01 23:35:46 -0300

    Update: Update database
    mudança do script js, aplicação de boas práticas

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def get():
 
 # Executa o servidor Flask
 if __name__ == '__main__':
-    Base.metadata.create_all(bind = engine)
+    Base.metadata.create_all(bind = engine) 
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,28 @@
-// Essa função atualiza os valores finais do gráfico somando os valores do eixo X, Y, Z e R.
-function updateFinalValues() {
-    // Seleciona os elementos de texto dos eixos X, Y, Z e R.
-    const inputs = ['#x', '#y', '#z', '#r'];
-    // Seleciona os elementos de texto dos valores finais dos eixos X, Y, Z e R.
-    const finals = ['#last_x', '#last_y', '#last_z', '#last_r'];
+class Values {
+    constructor(inputs, finals) {
+        this.inputs = inputs;
+        this.finals = finals;
+    }
 
-    // Percorre os elementos de texto dos eixos e soma seus valores aos valores finais correspondentes.
-    inputs.forEach((input, i) => {
+    updateFinalValues() {
+        this.inputs.forEach((input, i) => {
         const inputVal = parseInt($(input).text());
-        const finalVal = parseInt($(finals[i]).text()) + inputVal;
-        $(finals[i]).text(finalVal);
-    });
-}
+        const finalVal = parseInt($(this.finals[i]).text()) + inputVal;
+        $(this.finals[i]).text(finalVal);
+        });
+    }
 
-// Essa função envia os valores finais atualizados para o servidor através de uma requisição AJAX.
-function sendUpdatedValuesToServer() {
-    // Seleciona os elementos de texto dos valores finais dos eixos X, Y, Z e R.
-    const finalVals = ['#last_x', '#last_y', '#last_z', '#last_r'];
+    sendUpdatedValuesToServer() {
+        const data = this.finals.reduce((acc, curr) => {
+            acc[curr.slice(1)] = parseInt($(curr).text());
+            return acc;
+        }, {});
 
-    // Cria um objeto com os valores finais de cada eixo.
-    const data = finalVals.reduce((acc, curr) => {
-        acc[curr.slice(1)] = parseInt($(curr).text());
-        return acc;
-    }, {});
-
-    // Realiza uma requisição AJAX POST para enviar os dados para o servidor.
-    $.ajax({
-        url: '/data',
-        type: 'POST',
-        data: data,
-        success: function(response) {
+        $.ajax({
+            url: '/data',
+            type: 'POST',
+            data: data,
+            success: function(response) {
             alert('Dados enviados com sucesso!');
             location.reload();
         },
@@ -37,17 +30,32 @@ function sendUpdatedValuesToServer() {
             alert('Erro ao enviar dados: ' + error);
             location.reload();
         }
-    });
+        });
+    }
+
+    init() {
+        this.updateFinalValues();
+        $('#update').click(() => {
+            this.updateFinalValues();
+            this.sendUpdatedValuesToServer();
+        });
+    }
 }
 
-$(document).ready(function() {
-    // Chama a função que atualiza os valores finais quando a página é carregada.
-    updateFinalValues();
+class App {
+    constructor() {
+        this.Values = new Values(
+            ['#x', '#y', '#z', '#r'],
+            ['#last_x', '#last_y', '#last_z', '#last_r']
+        );
+    }
 
-    // Adiciona um listener de clique no botão de atualização.
-    $('#update').click(function() {
-        // Chama a função que atualiza os valores finais e envia os dados para o servidor.
-        updateFinalValues();
-        sendUpdatedValuesToServer();
-    });
+    init() {
+        this.Values.init();
+    }
+}
+
+$(document).ready(() => {
+    const app = new App();
+    app.init();
 });
